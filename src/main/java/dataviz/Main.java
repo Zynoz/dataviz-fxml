dbbb9913cd367e6d7e0f4045bec4ee1aa2698906
Maximilian Moser <maximilianmoser1997@gmail.com>
2020-01-13 18:04:36 +0100

    added about page changed application title

--- src/main/java/dataviz/Main.java
+++ src/main/java/dataviz/Main.java
@@ -4,19 +4,28 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/base_view.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 700, 600));
-        primaryStage.show();
+    public static void alert(Alert.AlertType alertType, String text, String header) {
+        Alert alert = new Alert(alertType);
+        alert.setHeaderText(header);
+        alert.setContentText(text);
+        alert.setTitle(alertType.name());
+        alert.showAndWait();
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/base_view.fxml"));
+        primaryStage.setTitle("DataViz");
+        primaryStage.setScene(new Scene(root, 700, 600));
+        primaryStage.show();
     }
 }
--- src/main/java/dataviz/base/BaseController.java
+++ src/main/java/dataviz/base/BaseController.java
@@ -1,5 +1,6 @@
 package dataviz.base;
 
+import dataviz.Main;
 import dataviz.index.IndexController;
 import dataviz.locking.LockingController;
 import dataviz.memory.MemoryController;
@@ -8,12 +9,19 @@ import dataviz.transaction.TransactionController;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.MenuItem;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class BaseController implements Initializable {
+
+    @FXML
+    public MenuItem reset;
+
+    @FXML
+    public MenuItem about;
 
     @FXML
     private TransactionController transactionController;
@@ -39,5 +47,6 @@ public class BaseController implements Initializable {
 
     private void addListeners() {
         close.setOnAction(e -> Platform.exit());
+        about.setOnAction(e -> Main.alert(Alert.AlertType.INFORMATION, "Created by:\n\tDenoise Stancov\n\tDenis Pelz\n\tAbraham Sedra\n\tMaximilian Moser\n\nVersion:\n\t0.1 pre-alpha", "About this Application"));
     }
 }
