8d59a5d68d9f5f0706ae093376b05f8c71fb79e6
Dennis <dennis.stancov@outlook.com>
2020-01-14 14:13:56 +0100

    fxml-injection and fx:ids created

--- src/main/java/dataviz/locking/LockingController.java
+++ src/main/java/dataviz/locking/LockingController.java
@@ -1,4 +1,47 @@
 package dataviz.locking;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
+import javafx.scene.text.Font;
+
 public class LockingController {
+
+    @FXML
+    private TextArea user1, user2, user3;
+
+    @FXML
+    private TableView tableView;
+
+    @FXML
+    private TableColumn c1, c2;
+
+    @FXML
+    private Circle circle1, circle2, circle3, circle4;
+
+    @FXML
+    private Polygon polygon1, polygon2, polygon3, polygon4;
+
+    @FXML
+    private Line line1, line2;
+
+    @FXML
+    private ScrollBar scrollBar1;
+
+    @FXML
+    private Label labelDeadlock, labelLock;
+
+    @FXML
+    private Font fontDeadlock, fontLock;
+
+    @FXML
+    private ImageView imageView1, imageView2, imageView3;
+
+    @FXML
+    private Image image1, image2, image3;
+
 }
--- src/main/java/dataviz/memory/MemoryController.java
+++ src/main/java/dataviz/memory/MemoryController.java
@@ -1,5 +1,35 @@
 package dataviz.memory;
 
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+
 public class MemoryController {
+
+    @FXML
+    private TextArea user1, user2, user3;
+
+    @FXML
+    private GridPane gridPane1, gridPane2;
+
+    @FXML
+    private ColumnConstraints colConstraint1, colConstraint2, colConstraint3, colConstraint4;
+
+    @FXML
+    private RowConstraints rowConstraint1, rowConstraint2, rowConstraint3, rowConstraint4;
+
+    @FXML
+    private ScrollBar scrollBar1, scrollBar2;
+
+    @FXML
+    private Label labelSGA, labelHDD;
+
+    @FXML
+    private Insets insetLeft1, insetLeft2;
 
 }
