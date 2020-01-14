194b33878bc5995124d2a5f34d2309099fcfdb85
Maximilian Moser <maximilianmoser1997@gmail.com>
2020-01-14 13:06:13 +0100

    removed test feature for prototype

--- src/main/java/dataviz/transaction/TransactionController.java
+++ src/main/java/dataviz/transaction/TransactionController.java
@@ -1,28 +1,17 @@
 package dataviz.transaction;
 
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class TransactionController implements Initializable {
 
-    @FXML
-    private Button button1;
-
-    @FXML
-    private Label counter;
-
-    private int count = 0;
-
     public void initialize(URL location, ResourceBundle resources) {
         addListeners();
     }
 
     private void addListeners() {
-        button1.setOnAction(e -> counter.setText(String.valueOf(++count)));
+
     }
 }
