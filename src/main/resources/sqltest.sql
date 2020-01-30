6724261fd566f1f1d86d480b59f7643d7b7f85ef
Zynoz <maximilianmoser1997@gmail.com>
2020-01-30 13:04:40 +0100

    Create sqltest.sql

---
+++ src/main/resources/sqltest.sql
@@ -0,0 +1,8 @@
+select * from test;
+insert into test (v1, v2, v3) values (value1, value2, value3);
+insert into test (v1, v2, v3) values (value1, value2, value3);
+insert into test (v1, v2, v3) values (value1, value2, value3);
+insert into test (v1, v2, v3) values (value1, value2, value3);
+delete from test where id = 4;
+update test set v2 = 'Maxi' where id = 6;
+update test set v2 = 3 where id = 6;
