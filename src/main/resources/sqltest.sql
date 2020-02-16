71783768bb8539e5f651ac088ea12a8d7bd3cd71
Maximilian Moser <maximilianmoser1997@gmail.com>
2020-02-16 21:40:30 +0100

    added test dta

--- src/main/resources/sqltest.sql
+++ src/main/resources/sqltest.sql
@@ -1,8 +1,29 @@
-select * from test;
-insert into test (v1, v2, v3) values (value1, value2, value3);
-insert into test (v1, v2, v3) values (value1, value2, value3);
-insert into test (v1, v2, v3) values (value1, value2, value3);
-insert into test (v1, v2, v3) values (value1, value2, value3);
-delete from test where id = 4;
-update test set v2 = 'Maxi' where id = 6;
-update test set v2 = 3 where id = 6;
+select *
+from test;
+insert into test (v1, v2, v3)
+values (value1, value2, value3);
+insert into test (v1, v2, v3)
+values (value1, value2, value3);
+insert into test (v1, v2, v3)
+values (value1, value2, value3);
+insert into test (v1, v2, v3)
+values (value1, value2, value3);
+delete
+from test
+where id = 4;
+update test
+set v2 = 'Maxi'
+where id = 6;
+update test
+set v2 = 3
+where id = 6;
+
+
+insert into test (id, name, amount, maxamount)
+values (1, Maxi, 10, 10);
+insert into test (id, name, amount, maxamount)
+values (2, Isabella, 5, 10);
+insert into test (id, name, amount, maxamount)
+values (3, Emilia, 16.6, 17.6);
+insert into test (id, name, amount, maxamount)
+values (4, Emma, 6, 10);
